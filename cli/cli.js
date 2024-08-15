70a4623f0bace7aadfa62f5aabf3d0f4534bad38
KOSASIH <kosasihg88@gmail.com>
2024-08-15 07:35:04 +0700

    Create cli.js

---
+++ cli/cli.js
@@ -0,0 +1,65 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function main() {
+  rl.question('Enter command: ', (cmd) => {
+    switch (cmd) {
+      case 'init':
+        initCmd();
+        break;
+      case 'run':
+        runCmd();
+        break;
+      case 'stop':
+        stopCmd();
+        break;
+      case 'status':
+        statusCmd();
+        break;
+      default:
+        console.log(`Unknown command: ${cmd}`);
+    }
+  });
+}
+
+function initCmd() {
+  console.log('Initializing...');
+  // initialization code here
+}
+
+function runCmd() {
+  console.log('Running...');
+  // running code here
+}
+
+function stopCmd() {
+  console.log('Stopping...');
+  // stopping code here
+}
+
+function statusCmd() {
+  console.log('Status:');
+  // status code here
+}
+
+function readLine(prompt) {
+  return new Promise((resolve) => {
+    rl.question(prompt, (line) => {
+      resolve(line);
+    });
+  });
+}
+
+function readInt(prompt) {
+  return readLine(prompt).then((line) => parseInt(line, 10));
+}
+
+function readFloat(prompt) {
+  return readLine(prompt).then((line) => parseFloat(line));
+}
+
+main();
