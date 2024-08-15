ff16fe867db51eb312d95b987ca9bfb76a21f075
KOSASIH <kosasihg88@gmail.com>
2024-08-15 08:25:09 +0700

    Create pi_prime_model.py

---
+++ models/pi_prime_model.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+class PiPrimeModel:
+    def __init__(self, num_bits):
+        self.num_bits = num_bits
+        self.model = tf.keras.models.Sequential([
+            tf.keras.layers.Dense(128, activation='relu', input_shape=(num_bits,)),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(1, activation='sigmoid')
+        ])
+        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    def predict(self, inputs):
+        outputs = self.model.predict(inputs)
+        return outputs
+
+    def train(self, inputs, labels):
+        self.model.fit(inputs, labels, epochs=10, batch_size=32)
