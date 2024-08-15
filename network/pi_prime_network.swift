fe5d9dbff07e8861bf71aa19458264baf83186c4
KOSASIH <kosasihg88@gmail.com>
2024-08-15 10:35:36 +0700

    Create pi_prime_network.swift

---
+++ network/pi_prime_network.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+class PiPrimeNetwork {
+    let socket: Socket
+
+    init(port: Int) {
+        socket = Socket()
+        socket.bind(to: port)
+        socket.listen()
+    }
+
+    func listen() {
+        while true {
+            let client = socket.accept()
+            handleConnection(client: client)
+        }
+    }
+
+    private func handleConnection(client: Socket) {
+        let input = client.recv(16)
+        let input = Int(input!.withUnsafeBytes { $0.load(as: UInt.self) })
+        let output = isPrime(input!)
+        client.send([output].withUnsafeBytes { $0 })
+    }
+
+    private func isPrime(_ n: Int) -> Bool {
+        if n < 2 { return false }
+        for i in 2...Int(sqrt(Double
