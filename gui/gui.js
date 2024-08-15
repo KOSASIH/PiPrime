8ab86dc3d343ee50d2bab18d8df91bef9bd7ea26
KOSASIH <kosasihg88@gmail.com>
2024-08-15 07:49:15 +0700

    Create gui.js

---
+++ gui/gui.js
@@ -0,0 +1,34 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL(`file://${__dirname}/index.html`);
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
